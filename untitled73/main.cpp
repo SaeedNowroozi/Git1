e509a61e25fefd9d3f3ecc54f87e5f30ceb95426
SaeedNowroozi <saeednowroozi69@gmail.com>
2021-06-05 16:36:50 +0430

    main change!

--- untitled73/main.cpp
+++ untitled73/main.cpp
@@ -1,8 +1,15 @@
 #include <QCoreApplication>
+#include <iostream>
+#include <QVector>
+#include <QDebug>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    qDebug() << "dsdsdsd";
 
     return a.exec();
 }
